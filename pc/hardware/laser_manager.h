a7f228a20ca08ce744d009e3e9d174d9584d293d
Emirhan <emirhanak2344@gmail.com>
2026-02-20 21:31:24 +0300

    fix: add missing laser_manager.h to repo, update .gitignore

---
+++ pc/hardware/laser_manager.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <string>
+#include "InterfaceLLT_2.h"
+
+class LaserManager {
+public:
+    LaserManager(const std::string& dllPath = "LLT.dll");
+    ~LaserManager();
+    bool init();
+    bool connect();
+    void startAcquisition();
+    void stopAcquisition();
+
+private:
+    std::string m_dllPath;
+    CInterfaceLLT* m_llt;
+    bool m_connected;
+};
