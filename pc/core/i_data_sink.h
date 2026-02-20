243c9ced641ddd813e65d7a2243b7f6cf2b959c9
Emirhan <emirhanak2344@gmail.com>
2026-02-20 21:16:58 +0300

    refactor: introduce IDataSink interface, decouple McuListener from stdout

---
+++ pc/core/i_data_sink.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include <cstdint>
+
+// Soyut veri alici arayuzu
+// McuListener, LaserManager gibi uretici siniflar bu arayuzu kullanir
+// Boylece stdout, dosya, kuyruk gibi farkli hedefler kolayca takilabilir
+struct IDataSink {
+    virtual void on_packet(const uint8_t* data, size_t size, uint64_t ts_ns) = 0;
+    virtual ~IDataSink() = default;
+};
